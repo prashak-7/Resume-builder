8cdc3f01ccbec6b824065ea018fc2a131a4e12e1
prashak-7 <prashantpathak175@gmail.com>
2025-09-17 20:21:30 +0545

    fix - modify db url

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   await mongoose
     .connect(
-      "mongodb+srv://prash:resume123@cluster0.tksehls.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+      process.env.MONGODB_URL
     )
     .then(() => console.log("Database connected"));
 };
